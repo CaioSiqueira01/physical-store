78e4e6d112165cd3601d1986135b6a99a6468e9a
CaioSiqueira01 <caiohenriquessiqueira.caike@gmail.com>
2025-02-27 02:11:01 -0300

    feat: creating project database

---
+++ src/config/database.js
@@ -0,0 +1,33 @@
+const sqlite3 = require("sqlite3").verbose();
+const path = require("path");
+
+// Caminho do banco de dados dentro da pasta "data"
+const dbPath = path.join(__dirname, "../../data/stores.db");
+
+// Criar uma conexão com o banco de dados
+const db = new sqlite3.Database(dbPath, (error) => {
+    if (error) {
+        console.error("Erro ao conectar ao banco de dados:", error.message);
+    } else {
+        console.log("Conectado ao banco de lojas!");
+    }
+});
+
+// Criar a tabela "stores" se ela não existir
+db.serialize(() => {
+    db.run(`
+       CREATE TABLE IF NOT EXISTS stores (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            rua TEXT NOT NULL,
+            numero TEXT NOT NULL,
+            bairro TEXT NOT NULL,
+            cidade TEXT NOT NULL,
+            estado TEXT NOT NULL,
+            cep TEXT NOT NULL,
+            latitude REAL,
+            longitude REAL 
+    `);
+});
+
+module.exports = db;
